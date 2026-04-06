46f217c16f035631ae990889f0c943e9334e96c8
helandyelduarte-code <helandyelduarte@gmail.com>
2026-04-06 11:02:52 -0500

    subindo novo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from './menu/menu';
+import { Locais } from './locais/locais';
 
 @Component({
   selector: 'app-root',
